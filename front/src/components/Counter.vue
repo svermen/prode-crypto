af284e751040b1db345a48a1afdb1c23f6400efb
Eric <inux2012@gmail.com>
2023-08-15 10:33:54 -0300

    initial front

---
+++ front/src/components/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="test">Counter {{ counter }}</div>
+  <button @click="incrementCounter">+1</button>
+</template>
+<script setup>
+  import { ref } from "vue"
+  const counter = ref(0)
+
+  const incrementCounter = () => {
+    counter.value += 1
+  }
+</script>
